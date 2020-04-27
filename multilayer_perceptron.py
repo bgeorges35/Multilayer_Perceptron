94eb540b9475e3d4446b924d00a0d8aad0c04480
Benoit <benoit.georges@6tm.com>
2020-04-27 15:18:50 +0200

    add neural network

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -5,6 +5,219 @@ Created on Mon Apr 27 11:29:33 2020
 
 @author: benoitgeorges
 """
+import pandas as pd
+import numpy as np
+import seaborn as sns;
+import matplotlib.pyplot as plt
+
+def preprocessing_data(df):
+    X = df.iloc[:, 2:]
+    X = np.array(X)
+    X = (X - np.min(X)) / (np.max(X) - np.min(X))
+    X = X.T
+    Y = df.iloc[:, 1]
+    Y = np.array(Y)
+    Y = np.where(Y == 'B', 0, 1)
+    Y = Y.reshape((1, len(Y)))
+    return X, Y
+
+def initialize_parameters(layer_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)            # number of layers in the network
+    for l in range(1, L):
+        ### START CODE HERE ### (≈ 2 lines of code)
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * np.sqrt(2 / layer_dims[l - 1])
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        ### END CODE HERE ###
+        
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+        
+    return parameters
+
+
+def relu(Z):
+    return np.maximum(Z, 0)
+
+def softmax(Z):
+    return (np.exp(Z) / np.sum(np.exp(Z), axis=0))
+
+def sigmoid(Z):
+    return 1 / (1 + np.exp(-Z))
+
+def linear_activation_forward(A_prev, W, b, activation):    
+
+    Z = np.dot(W, A_prev) + b
+    if activation == "softmax":
+        A = softmax(Z)
+    elif activation == "sigmoid":
+        A = sigmoid(Z)
+        
+    elif activation == "relu":
+        A = relu(Z)
+    cache = ((A, W, b), Z)
+    
+    print(A.shape)
+    return A, cache
+
+def L_model_forward(X, parameters):
+    caches = []
+    A = X
+    L = len(parameters) // 2 
+    for l in range(1, L):
+        A_prev = A
+        A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], "relu")
+        caches.append(cache)
+    AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], "sigmoid")
+    caches.append(cache)
+    assert(AL.shape == (1,X.shape[1]))
+            
+    return AL, caches
+
+def compute_cost(AL, Y):
+    m = Y.shape[1]
+    cost = (-1/m) * np.sum(Y * np.log(AL) + (1 - Y) * np.log(1 - AL), axis= 1, keepdims=True)
+
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    return cost
+
+def sigmoid_backward(dA, Z):
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    return dZ
+
+def relu_backward(dA, Z):
+    # just converting dz to a correct object.
+    dZ = np.array(dA, copy=True)
+    # When z <= 0, we should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+    return dZ    
+def softmax_backward(linear_cache):
+    A = linear_cache
+    # -SjSi can be computed using an outer product between Sz and itself. Then
+    # we add back Si for the i=j cases by adding a diagonal matrix with the
+    # values of Si on its diagonal.
+    D = -np.outer(A, A) + np.diag(A.flatten())
+    return D
+    
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+    dW = 1/m * np.dot(dZ, A_prev.T)
+    db = 1/m * np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(W.T, dZ)
+    
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+    
+    return dA_prev, dW, db
+
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, Z = cache
+    if activation == "relu":
+        dZ = relu_backward(dA, Z)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+        ### END CODE HERE ###
+        
+    elif activation == "sigmoid":
+        dZ = sigmoid_backward(dA, Z)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+        
+    elif activation == "softmax":
+        dZ = softmax_backward(linear_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    
+    return dA_prev, dW, db
+
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = len(caches) # the number of layers
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
+
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+    current_cache = caches[L-1]
+    grads["dA" + str(L-1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache, activation = "sigmoid")
+    
+    for l in reversed(range(L-1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 1)], current_cache, 'relu')
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+
+    return grads
+
+def update_parameters(parameters, grads, learning_rate):
+    
+    L = len(parameters) // 2 # number of layers in the neural network
+
+    for l in range(L):
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * grads["dW" + str(l + 1)]
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * grads["db" + str(l + 1)]
+
+    return parameters
+
+def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):#lr was 0.009
+
+    np.random.seed(1)
+    costs = []                         # keep track of cost
+    
+    # Parameters initialization. (≈ 1 line of code)
+    ### START CODE HERE ###
+    parameters = initialize_parameters(layers_dims)
+    ### END CODE HERE ###
+    
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        ### START CODE HERE ### (≈ 1 line of code)
+        AL, caches = L_model_forward(X, parameters)
+        ### END CODE HERE ###
+        
+        # Compute cost.
+        ### START CODE HERE ### (≈ 1 line of code)
+        cost = compute_cost(AL, Y)
+        ### END CODE HERE ###
+    
+        # Backward propagation.
+        ### START CODE HERE ### (≈ 1 line of code)
+        grads = L_model_backward(AL, Y, caches)
+        ### END CODE HERE ###
+ 
+        # Update parameters.
+        ### START CODE HERE ### (≈ 1 line of code)
+        parameters = update_parameters(parameters, grads, learning_rate)
+        ### END CODE HERE ###
+                
+        # Print the cost every 100 training example
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+            
+    # plot the cost
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
 
 if __name__ == '__main__':
+    df=pd.read_csv('data.csv', header=None)
+    X, Y = preprocessing_data(df)
+    # print(Y.shape)
+    # print(X.shape)
+    layers_dims = [X.shape[0], 4, 3, 1]
+    L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False)
     
+    
+        
